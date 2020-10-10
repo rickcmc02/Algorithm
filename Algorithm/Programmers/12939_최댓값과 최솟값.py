e80aa54b753e64c66179ffcd4a23a901c5e8c0a4
양희영 <aa87565115@gmail.com>
2020-10-10 15:26:23 +0900

    easy algo add

---
+++ Algorithm/Programmers/12939_최댓값과 최솟값.py
@@ -0,0 +1,20 @@
+'''
+문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 (최소값) (최대값)형태의 문자열을 반환하는 함수, solution을 완성하세요.
+예를들어 s가 1 2 3 4라면 1 4를 리턴하고, -1 -2 -3 -4라면 -4 -1을 리턴하면 됩니다.
+
+제한 조건
+s에는 둘 이상의 정수가 공백으로 구분되어 있습니다.
+'''
+
+def solution(s):
+    answer = ''
+    sLst = []
+    mMLst = []
+    
+    sLst = list(map(int, s.split(' ')))
+    
+    mMLst = [min(sLst), max(sLst)]
+    mMLst = list(map(str, mMLst))
+    
+    answer = ' '.join(mMLst)
+    return answer
