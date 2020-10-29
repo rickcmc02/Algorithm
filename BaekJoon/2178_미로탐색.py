213997c3eb711f75ab38b167451ba2f08e224cc1
양희영 <aa87565115@gmail.com>
2020-10-29 22:51:47 +0900

    escape underdev

--- BaekJoon/2178_미로탐색.py
+++ BaekJoon/2178_미로탐색.py
@@ -1,12 +1,10 @@
 '''
 N×M크기의 배열로 표현되는 미로가 있다.
-
 1	0	1	1	1	1
 1	0	1	0	1	0
 1	0	1	0	1	1
 1	1	1	0	1	1
 미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
-
 위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.
 
 입력
@@ -27,10 +25,8 @@ dy = [-1, 0, 1, 0]
 dx = [0, -1, 0, 1]
 dq = deque()
 
-def bfs(y, x):
-    dq.append([y, x])
-    visited[y][x] = 1
-    
+def bfs():
+    dq.append([0, 0])
     while dq :
         pos = dq.popleft()
 
@@ -41,10 +37,5 @@ def bfs(y, x):
                 maze[ny][nx] = maze[pos[0]][pos[1]] + 1
                 visited[ny][nx] = 1
                 dq.append([ny, nx])
-
-for i in range(v) :
-    for j in range(h) :
-        if not visited[i][j] :
-            bfs(i,j)
-
+bfs()
 print(maze[-1][-1])
