d3563703818f26227a4e8e906459c00b9ece1d40
rickmcc02 <aa87565115@gmail.com>
2020-11-13 10:32:01 +0900

    target num finished

--- Programmers/43165_타겟넘버.py
+++ Programmers/43165_타겟넘버.py
@@ -13,32 +13,31 @@ n개의 음이 아닌 정수가 있습니다. 이 수를 적절히 더하거나 
 각 숫자는 1 이상 50 이하인 자연수입니다.
 타겟 넘버는 1 이상 1000 이하인 자연수입니다.
 '''
-# developing
 
-def plus_minus(n_list, s_nbs, tgt):
-    sum_list = [0]
+def plus_minus(num_l, sum_n, tgt):
+    sum_list = [sum_n]
+    diff = sum_n - tgt
     
-    while n_list[-1] * 2 > diff :
-        n_list.pop()
+    while num_l[-1] * 2 > diff :
+        num_l.pop()
 
-    for i in range(len(n_list)) :
-        temp_list = [0]
+    for i in range(len(num_l)) :
+        temp_l = [0]
         for j in range(len(sum_list)) :
-            temp_list.append(sum_list[j] - n_list[i])
-            temp_list.append(sum_list[j] + n_list[i])
-        sum_list = temp_list
-    
-    return sum_list.count(diff)
+            temp_l.append(sum_list[j] - 2*num_l[i])
+            temp_l.append(sum_list[j])
+        sum_list = temp_l
+    return sum_list.count(tgt)
 
-def solution(numbers, target):
-    numbers.sort()
+def solution(numbers_list, target):
+    numbers_list.sort()
     
-    sum_nbs = sum(numbers)
-    if sum_nbs < target :
+    sum_numbers = sum(numbers_list)
+    if sum_numbers < target :
         return 0
-    elif sum_nbs == target :
+    elif sum_numbers == target :
         return 1
     else :
-        return plus_minus(numbers, sum_nbs, target)
+        return plus_minus(numbers_list, sum_numbers, target)
 
-print(solution([1, 1, 1, 1, 1], 3))
+# print(solution([1, 1, 1, 1, 1], 3))
