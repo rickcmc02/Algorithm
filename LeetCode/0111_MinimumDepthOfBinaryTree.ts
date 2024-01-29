4a4c9dc560e7a3e2944b2aee501b1afee845dbc9
양희영 <aa87565115@gmail.com>
2024-01-30 00:49:33 +0900

    leet: 0111 solved

---
+++ LeetCode/0111_MinimumDepthOfBinaryTree.ts
@@ -0,0 +1,42 @@
+/*
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+Example 2:
+
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 105].
+-1000 <= Node.val <= 1000
+*/
+
+function minDepth(root: TreeNode | null): number {
+  let depth = 0;
+  if (!root) return depth;
+
+  function checkDepth(tn: TreeNode, lv: number): void {
+    if (!tn?.left && !tn?.right) {
+      if (!depth || depth > lv + 1) depth = lv + 1;
+    } else {
+      if (tn.left) checkDepth(tn.left, lv + 1);
+      if (tn.right) checkDepth(tn.right, lv + 1);
+      return;
+    }
+  }
+  checkDepth(root, depth);
+  return depth;
+}
