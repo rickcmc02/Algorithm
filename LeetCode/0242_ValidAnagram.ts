7cf34a01a34f642d3e2eba9c74481b813b530fe8
rickcmc02 <aa87565115@gmail.com>
2024-03-24 00:10:21 +0900

    leet: 0242 solved

--- LeetCode/0242_ValidAnagram.ts
+++ LeetCode/0242_ValidAnagram.ts
@@ -20,3 +20,28 @@ Constraints:
 1 <= s.length, t.length <= 5 * 104
 s and t consist of lowercase English letters.
 */
+
+function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+
+  let isAnagram = true;
+  const sDict = {};
+  for (const sLetter of s) {
+    const matchedCount = sDict[sLetter];
+    if (matchedCount) sDict[sLetter] = matchedCount + 1;
+    else sDict[sLetter] = 1;
+  }
+
+  for (const tLetter of t) {
+    const matchedCount = sDict[tLetter];
+    if (matchedCount) {
+      if (matchedCount === 1) delete sDict[tLetter];
+      else sDict[tLetter] = matchedCount - 1;
+    } else {
+      isAnagram = false;
+      break;
+    }
+  }
+
+  return isAnagram;
+}
