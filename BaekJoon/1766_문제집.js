c897158d7777272e50ea90d000451f3476f3551a
rickcmc02 <aa87565115@gmail.com>
2024-06-09 21:38:27 +0900

    baek: 1766 under progress

---
+++ BaekJoon/1766_문제집.js
@@ -0,0 +1,58 @@
+// const input = require("fs")
+//   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+//   .toString()
+//   .trim()
+//   .split(" ")
+//   .map(Number);
+
+const rawInput = `6 7
+5 6
+5 2
+2 1
+6 1
+2 4
+1 3
+4 3
+`;
+
+const input = rawInput
+  .toString()
+  .trim()
+  .split("\n")
+  .map((e) => e.split(" ").map(Number));
+
+const [N, M] = input[0];
+const visited = Array(N + 1).fill(0);
+const formerData = {};
+
+for (let formerInfoIdx = 1; formerInfoIdx <= M; formerInfoIdx++) {
+  const [former, latter] = input[formerInfoIdx];
+  formerData[latter] = former; // 특정 문제에 대해서 선행 문제가 하나일 때만 해당
+}
+
+const buildStr = (latter, addedStr) => {
+  visited[latter] = 1;
+  if (formerData[latter]) {
+    addedStr += buildStr(formerData[latter], latter + " " + addedStr);
+  } else {
+    addedStr = latter + " " + addedStr;
+    return addedStr;
+  }
+};
+
+let answer = "";
+for (let i = 1; i <= N; i++) {
+  if (visited[i]) continue;
+
+  let addedStr = "";
+  if (formerData[i]) {
+    addedStr += buildStr(formerData[i], i);
+  } else {
+    addedStr += i;
+    visited[i] = 1;
+  }
+  if (answer) answer += " " + addedStr;
+  else answer = addedStr;
+}
+
+console.log(answer);
