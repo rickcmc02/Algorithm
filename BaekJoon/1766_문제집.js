34a6ecafe20b37fd32da05a4de27a0e2f9d0ed17
rickcmc02 <aa87565115@gmail.com>
2024-06-13 00:28:42 +0900

    baek: 1766 wrong

--- BaekJoon/1766_문제집.js
+++ BaekJoon/1766_문제집.js
@@ -7,38 +7,53 @@
 
 // 최소힙, 위상정렬을 이용해 푸는 문제 - 위상정렬을 이용해야 하는 이유는 선행 문제가 여러개일 수 있기 때문
 
-const rawInput = `6 7
-5 6
-5 2
+const rawInput = `6 6
+6 5
+6 4
+5 3
+4 2
+3 1
 2 1
-6 1
-2 4
-1 3
-4 3
 `;
 
 const input = rawInput
   .toString()
   .trim()
   .split("\n")
-  .map((e) => e.split(" ").map(Number));
+  .map((str) => str.split(" ").map(Number));
 
 const [N, M] = input[0];
-const visited = Array(N + 1).fill(0);
+const visited = Array(N + 1).fill(0); // 방문 여부를 체크하기 위한 배열 (0: 방문하지 않음, 1: 방문함, 0번 인덱스는 사용하지 않음)
 const formerData = {};
 
 for (let formerInfoIdx = 1; formerInfoIdx <= M; formerInfoIdx++) {
   const [former, latter] = input[formerInfoIdx];
-  formerData[latter] = former; // 특정 문제에 대해서 선행 문제가 하나일 때만 해당
+  const formerList = formerData[latter];
+  if (formerList) formerData[latter] = formerList.concat(former);
+  else formerData[latter] = [former];
 }
 
-const buildStr = (latter, addedStr) => {
-  visited[latter] = 1;
-  if (formerData[latter]) {
-    addedStr += buildStr(formerData[latter], latter + " " + addedStr);
-  } else {
-    addedStr = latter + " " + addedStr;
-    return addedStr;
+for (let i = 1; i <= N; i++) {
+  if (formerData[i]) formerData[i].sort((a, b) => a - b);
+}
+
+const buildStr = (formerList, addedStr, idx) => {
+  if (idx === formerList.length) return addedStr;
+
+  const former = formerList[idx];
+  const isVisited = visited[former];
+  if (formerData[former]) {
+    if (isVisited) return buildStr(formerData[former], addedStr, 0);
+    else {
+      visited[former] = 1;
+      return buildStr(formerData[former], former + " " + addedStr, 0);
+    }
+  }
+
+  if (isVisited) return buildStr(formerList, addedStr, idx + 1);
+  else {
+    visited[former] = 1;
+    return buildStr(formerList, former + " " + addedStr, idx + 1);
   }
 };
 
@@ -48,11 +63,11 @@ for (let i = 1; i <= N; i++) {
 
   let addedStr = "";
   if (formerData[i]) {
-    addedStr += buildStr(formerData[i], i);
+    addedStr += buildStr(formerData[i], i, 0);
   } else {
     addedStr += i;
-    visited[i] = 1;
   }
+  visited[i] = 1;
   if (answer) answer += " " + addedStr;
   else answer = addedStr;
 }
