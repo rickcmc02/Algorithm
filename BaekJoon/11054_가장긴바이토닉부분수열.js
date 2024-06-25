412f6dd1d15b8a96b93e338031e50e9090b1140c
rickcmc02 <aa87565115@gmail.com>
2024-06-26 00:06:10 +0900

    baek: 11054 add comment

--- BaekJoon/11054_가장긴바이토닉부분수열.js
+++ BaekJoon/11054_가장긴바이토닉부분수열.js
@@ -5,25 +5,29 @@ const input = rawInput.toString().trim().split("\n");
 const N = +input[0];
 const numList = input[1].split(" ").map(Number);
 
-const ascDP = Array(N).fill(1);
+const ascDP = Array(N).fill(1); // 오름차순 동적 프로그래밍 배열
 const descDP = Array(N).fill(1);
 
 const minNum = 1;
 let longest = 1;
 
+// 2중 반복문으로
 for (let idx = 0; idx < N; idx++) {
   const currNum = numList[idx];
-  if (currNum === minNum) continue;
-  let currMax = 1;
+  if (currNum === minNum) continue; // 1이면 패스
+  let currMax = 1; // 현재 최대값
+  // 지금 idx 이전까지의 idx만 비교
   for (let compIdx = 0; compIdx < idx; compIdx++) {
     const compNum = numList[compIdx];
     if (currNum > compNum) {
+      // 현재 숫자가 비교 숫자보다 높으니까, +1을 해주는 것이 맞다
       currMax = Math.max(currMax, ascDP[compIdx] + 1);
     }
   }
   ascDP[idx] = currMax;
 }
 
+// 뒤에서부터 탐색
 for (let idx = N - 1; idx >= 0; idx--) {
   const currNum = numList[idx];
   if (currNum === minNum) continue;
