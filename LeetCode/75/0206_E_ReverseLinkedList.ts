7af7fca99d79b976625217d6f73368fbac35a0b3
rickcmc02 <aa87565115@gmail.com>
2025-04-19 23:17:55 +0900

    leet: 0206 solved

--- LeetCode/75/0206_E_ReverseLinkedList.ts
+++ LeetCode/75/0206_E_ReverseLinkedList.ts
@@ -25,3 +25,18 @@ The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000
 
 */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  if (!head) return null;
+  let currLN = head;
+  let revLN: ListNode = new ListNode();
+  let isStarted = false
+
+  while (currLN?.val || currLN?.val === 0) {
+      if (isStarted) revLN.next = { val: revLN.val, next: revLN.next };
+      else isStarted = true;
+      revLN.val = currLN.val;
+      currLN = currLN.next as ListNode;
+  }
+  return revLN;
+};
