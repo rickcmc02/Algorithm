83716126040cf22069837be6ab100b7461d88f44
rickcmc02 <aa87565115@gmail.com>
2025-05-30 00:07:03 +0900

    leet: 0994 solved

--- LeetCode/75/0994_M_RottingOranges.ts
+++ LeetCode/75/0994_M_RottingOranges.ts
@@ -35,3 +35,45 @@ n == grid[i].length
 grid[i][j] is 0, 1, or 2.
 
 */
+
+const DIRECTIONS = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+
+function orangesRotting(grid: number[][]): number {
+    const [m, n] = [grid.length, grid[0].length]; // m: y방향, n: x방향
+    const visited = Array.from({ length: m }, () => new Array(n).fill(false));
+    const queue: [number, number, number][] = []; // [y, x, minute]
+    let minute = 0;
+    let numFresh = 0;
+
+    const checkFresh = (y, x) => y >= 0 && y < m && x >= 0 && x < n && grid[y][x] === 1;
+
+    for (let y = 0; y < m; y++) {
+        for (let x = 0; x < n; x++) {
+            if (grid[y][x] === 2) {
+                queue.push([y, x, 0]);
+                visited[y][x] = true;
+            }
+
+            if (grid[y][x] === 1) {
+                numFresh++;
+            }
+        }
+    }
+
+    while (queue.length) {
+        const [y, x, min] = queue.shift()!;
+        if (min > minute) minute = min;
+
+        for (const [dy, dx] of DIRECTIONS) {
+            const [ny, nx] = [y + dy, x + dx];
+            const isOrange = checkFresh(ny, nx);
+            if (isOrange) {
+                queue.push([ny, nx, min + 1]);
+                grid[ny][nx] = 2;
+                numFresh--; // 썩을때마다 신선한 오렌지 -1
+            }
+        }
+    }
+
+    return numFresh ? -1 : minute; // 썩히지 못한 오렌지 있으면 -1
+};
