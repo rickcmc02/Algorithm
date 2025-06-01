42265ba9888d3bcb8ae6b492dd7289243fdc45bf
rickcmc02 <aa87565115@gmail.com>
2025-06-02 00:05:08 +0900

    leet: 0215 modified

--- LeetCode/75/0215_M_KthLargestElementInAnArray.ts
+++ LeetCode/75/0215_M_KthLargestElementInAnArray.ts
@@ -24,89 +24,76 @@ Constraints:
 
 */
 
-// first solution
+// second solution
 
 class MaxHeap {
-  heap = []; // 힙을 저장할 배열
+  heap: number[] = [];
 
-  constructor() {
-  }
-
-  // 힙의 크기를 반환하는 메서드
   size() {
       return this.heap.length;
   }
 
-  // 두 값을 바꿔주는 메서드
-  swap(idx1, idx2) {
+  add(n: number) {
+      this.heap.push(n);
+      this.bubbleUp();
+  }
+
+  swap(idx1: number, idx2: number) {
       [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
   }
 
-  // 새로운 노드를 추가하는 메서드
-  add(value) {
-      this.heap.push(value); // 힙의 끝에 새로운 노드 추가
-      this.bubbleUp(); // 새로운 값이 추가된 위치에서 bubbleUp() 수행
-  }
-
-  // 끝에 추가된 노드를 부모와 비교하며 자리 찾아가기
   bubbleUp() {
-      let index = this.size() - 1; // 새로운 노드가 추가된 위치
-      let parentIdx = Math.floor((index - 1) / 2); // 부모 노드의 위치
-      while (
-          (this.heap[parentIdx] || this.heap[parentIdx] === 0) && // 부모 노드가 존재하고 (0 포함)
-          this.heap[index] > this.heap[parentIdx] // 새로운 노드가 부모 노드보다 큰 경우
-      ) {
-          this.swap(index, parentIdx); // 두 노드의 값을 교체
-          index = parentIdx; // 인덱스를 부모 노드의 인덱스로 변경
-          parentIdx = Math.floor((index - 1) / 2); // 새로운 부모 노드의 인덱스 계산
+      let idx = this.size() - 1;
+      let parentIdx = Math.floor((idx - 1) / 2);
+
+      while (!isNaN(this.heap[parentIdx]) && this.heap[parentIdx] < this.heap[idx]) {
+          this.swap(idx, parentIdx);
+          idx = parentIdx;
+          parentIdx = Math.floor((idx - 1) / 2); // 새로운 부모 모드의 인덱스 계산
       }
   }
 
-  // 부모 노드를 자식과 비교하여 자리 찾아가기
   bubbleDown() {
-      let index = 0;
-      const length = this.size();
+      let idx = 0;
+      const len = this.size();
+
       while (true) {
-          let largest = index;
-          const left = 2 * index + 1;
-          const right = 2 * index + 2;
+          let largest = idx;
+          const [left, right] = [2 * idx + 1, 2 * idx + 2];
 
-          if (left < length && this.heap[left] > this.heap[largest]) {
-              largest = left;
+          if (left < len && this.heap[left] > this.heap[largest]) {
+              largest = left; // 왼쪽 자식 노드의 값보다 부모가 작음
+          }
+          if (right < len && this.heap[right] > this.heap[largest]) {
+              largest = right; // 오른쪽 자식 노드의 값보다 왼쪽 자식 노드 또는 부모가 작음
           }
 
-          if (right < length && this.heap[right] > this.heap[largest]) {
-              largest = right;
-          }
-
-          if (largest === index) break;
-          this.swap(index, largest);
-          index = largest;
+          if (largest === idx) break; // 부모가 자식보다 큼
+          this.swap(idx, largest);
+          idx = largest;
       }
+
   }
 
-  poll(): number {
+  poll() {
       if (this.size() === 0) return -1;
-      if (this.size() === 1) return this.heap.pop()!; // 최대값 꺼내기
       const max = this.heap[0];
-      this.heap[0] = this.heap.pop()!; // 루트에 마지막 넣고
-      this.bubbleDown(); // 재정렬
+      this.heap[0] = this.heap.pop()!; // 루트에 마지막 값 삽입
+      this.bubbleDown(); // 루트 자식 노드 비교하면서 재정렬
       return max;
   }
 }
 
 function findKthLargest(nums: number[], k: number): number {
   const maxHeap = new MaxHeap();
-
-  // 전체 수를 힙에 넣음 (O(N log N))
+  
   for (const num of nums) {
       maxHeap.add(num);
   }
 
-  // k - 1번 꺼냄 (O(K log N))
   for (let i = 1; i < k; i++) {
-      const popped = maxHeap.poll(); // 가장 큰 수 제거
+      maxHeap.poll(); // k - 1번 가장 큰 수 제거
   }
 
-  return maxHeap.poll(); // k번째 큰 수
+  return maxHeap.poll();
 }
