1c7dac57a74003541ddc9449f344b8711d5ea66c
rickmcc02 <aa87565115@gmail.com>
2025-08-21 00:04:50 +0900

    leet: 0605 answer 2 added

--- LeetCode/75/0605_E_CanPlaceFlowers.ts
+++ LeetCode/75/0605_E_CanPlaceFlowers.ts
@@ -23,6 +23,30 @@ There are no two adjacent flowers in flowerbed.
 0 <= n <= flowerbed.length
 */
 
+// answer 2
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    const bedLen = flowerbed.length;
+    const newbed = new Array(bedLen).fill(0);
+    let numPlantable = 0;
+
+    for (let i = 0; i < bedLen; i++) {
+        const fb = flowerbed[i];
+        const nb = newbed[i];
+        const nbp = i ? newbed[i - 1] : 0; // new bed prev
+        const fbn = i === bedLen - 1 ? 0 : flowerbed[i + 1] // flower bed next
+
+        if (fb) newbed[i] = fb; // 심어 있던 곳은 그대로 심기
+        else if (!(nbp || fbn)) {
+            newbed[i] = 1; // 새 플랜드 앞, 기존 플랜트 뒤 값이 비어있다면 심을 수 있음
+            numPlantable++;
+        }
+    }
+
+    return numPlantable >= n;
+};
+
+// answer 1
+/*
 function canPlaceFlowers(flowerbed: number[], n: number): boolean {
   let prevSpot = 0;
   let nextSpot = 0;
@@ -41,3 +65,4 @@ function canPlaceFlowers(flowerbed: number[], n: number): boolean {
 
   return false;
 }
+*/
