a62ca513471dd394f354db294dc4a5f1ea850c8c
rickmcc02 <aa87565115@gmail.com>
2025-12-03 23:55:11 +0900

    leet: 1318 solved

--- LeetCode/75/1318_M_MinimumFlipsToMakeaORbEqualToC.ts
+++ LeetCode/75/1318_M_MinimumFlipsToMakeaORbEqualToC.ts
@@ -27,3 +27,33 @@ Constraints:
 1 <= b <= 10^9
 1 <= c <= 10^9
 */
+
+function minFlips(a: number, b: number, c: number): number {
+    const [aBin, bBin, cBin] = [a.toString(2), b.toString(2), c.toString(2)];
+    const [aLen, bLen, cLen] = [aBin.length, bBin.length, cBin.length];
+    const longest = Math.max(aLen, bLen, cLen);
+    let cnt = 0;
+
+    for (let i = 1; i <= longest; i++) {
+        const [aIdx, bIdx, cIdx] = [aLen - i, bLen - i, cLen - i];
+        const [isA, isB, isC] = [aIdx >= 0, bIdx >= 0, cIdx >= 0];
+        const aOne = isA && aBin[aIdx] === "1";
+        const bOne = isB && bBin[bIdx] === "1";
+
+        if (isC) {
+            const curr = cBin[cIdx];
+            if (curr === "1") {
+                // 둘 다 없는 경우
+                if (!aOne && !bOne) cnt++;
+            } else {
+                if (aOne) cnt++;
+                if (bOne) cnt++;
+            }
+        } else { // c보다 더 큰자리수에 a 또는 b의 값 존재
+            if (aOne) cnt++;
+            if (bOne) cnt++;
+        }
+    }
+
+    return cnt;
+};
